2804251a5fc41bc3ff531256533e8c1c23ed82ec
Thor <thorname@gmail.com>
2023-05-02 14:55:49 -0500

    Update Exercise

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Primera Aplicacion</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.mi.js"></script>
+</head>
+<body>
+    <form>
+        <div class="mb-3">
+            <label for="identificador" class="form-label">Identification</label>
+            <input type="text" class="form-control" id="identificador" name="identificador">
+        </div>
+        <div class="mb-3">
+            <label for="nombre" class="form-label">Name</label>
+            <input type="text" class="form-control" id="nombre">
+        </div>
+        <div class="mb-3">
+            <label for="detalle" class="form-label">Detail</label>
+            <input type="text" class="form-control" id="detalle">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>    
+</body>
+</html>
